94e09fee69a9e35017d82a105250de7d172b2eba
herodragon120 <phuctlq113@gmail.com>
2018-06-11 15:40:08 +0700

    Các mối quan hệ - Attribute

--- DAM_FW/DAM_FW/Attribute/OneToManyAttribute.cs
+++ DAM_FW/DAM_FW/Attribute/OneToManyAttribute.cs
@@ -4,9 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DAM_FW.Attribute
+namespace DAM_FW
 {
-    class OneToManyAttribute
+    public class OneToManyAttribute : System.Attribute
     {
+        private string _RelationshipID;
+        public string RelationshipID
+        {
+            get { return _RelationshipID; }
+            private set { _RelationshipID = value; }
+        }
+
+        private string _TableName;
+        public string TableName
+        {
+            get { return _TableName; }
+            private set { _TableName = value; }
+        }
+        public OneToManyAttribute(string relationshipid, string tablename)
+        {
+            this._RelationshipID = relationshipid;
+            this._TableName = tablename;
+        }
     }
 }
