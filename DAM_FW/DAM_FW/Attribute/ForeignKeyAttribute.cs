5f5231dc08a3af92016b547d1bb50b5f753f78d9
herodragon120 <phuctlq113@gmail.com>
2018-06-11 15:34:36 +0700

    Update primary+foreignkey

--- DAM_FW/DAM_FW/Attribute/ForeignKeyAttribute.cs
+++ DAM_FW/DAM_FW/Attribute/ForeignKeyAttribute.cs
@@ -4,9 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DAM_FW.Attribute
+namespace DAM_FW
 {
-    class ForeignKeyAttribute
+    public class ForeignKeyAttribute : System.Attribute
     {
+        private string _RelationshipID;
+        public string RelationshipID
+        {
+            get { return _RelationshipID; }
+            private set { _RelationshipID = value; }
+        }
+
+        private string _Name;
+        public string Name
+        {
+            get { return _Name; }
+            private set { _Name = value; }
+        }
+
+        private string _References;
+        public string References
+        {
+            get { return _References; }
+            private set { _References = value; }
+        }
+
+        public ForeignKeyAttribute(string relationshipid, string name , string references)
+        {
+            this._Name = name;
+            this._RelationshipID = relationshipid;
+            this._References = references;
+        }
     }
 }
--- DAM_FW/DAM_FW/Attribute/PrimaryKeyAttribute.cs
+++ DAM_FW/DAM_FW/Attribute/PrimaryKeyAttribute.cs
@@ -4,9 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DAM_FW.Attribute
+namespace DAM_FW
 {
-    class PrimaryKeyAttribute
+    public class PrimaryKeyAttribute : System.Attribute
     {
+        private string _Name;
+        public string Name
+        {
+            get { return _Name; }
+            private set { _Name = value; }
+        }
+
+        private bool _AutoID;
+        public bool AutoID
+        {
+            get { return _AutoID; }
+            private set { _AutoID = value; }
+        }
+        PrimaryKeyAttribute (string name , bool autoid)
+        {
+            this._AutoID = autoid;
+            this._Name = name;
+        }
     }
 }
